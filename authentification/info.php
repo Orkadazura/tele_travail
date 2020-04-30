1cbfb6bd04ef39d0f2122e7b094ff1576e3e6338
Orkadazura <53291088+Orkadazura@users.noreply.github.com>
2020-04-30 14:27:35 +0200

    Add files via upload

---
+++ authentification/info.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if(isset($_SESSION["login"]) && isset($_SESSION["pwd"]))
+{
+	echo "Bravo  M(e)".$_SESSION["login"]." vous êtes connecté(e)";
+}
+else
+{	
+	echo "Erreur vous devez recommencer!! ";
+}
+
+
+
+?>
+
+
